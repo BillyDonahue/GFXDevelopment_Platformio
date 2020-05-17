6e31b11d28f3ac0bbb9d2523b9a9e255d3de7e90
Billy Donahue <billy.donahue@gmail.com>
2020-05-17 01:38:20 -0400

    org_01 font

--- src/text_demo.cpp
+++ src/text_demo.cpp
@@ -24,38 +24,85 @@
 #include <Fonts/FreeMonoBold12pt7b.h>
 #include <Fonts/FreeMonoOblique9pt7b.h>
 #include <Fonts/FreeSerifItalic12pt7b.h>
+#include <Fonts/Org_01.h>
 #include <SPI.h>
 #include <Wire.h>
 
-#define SCREEN_WIDTH 128 // OLED display width, in pixels
-#define SCREEN_HEIGHT 32 // OLED display height, in pixels
-
-// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
-#define OLED_RESET 4 // Reset pin # (or -1 if sharing Arduino reset pin)
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
-
-#define NUMFLAKES 10 // Number of snowflakes in the animation example
-
-#define LOGO_HEIGHT 16
-#define LOGO_WIDTH 16
-const unsigned char PROGMEM logo_bmp[] = {B00000000, B11000000, //
-                                          B00000001, B11000000, //
-                                          B00000001, B11000000, //
-                                          B00000011, B11100000, //
-                                          B11110011, B11100000, //
-                                          B11111110, B11111000, //
-                                          B01111110, B11111111, //
-                                          B00110011, B10011111, //
-                                          B00011111, B11111100, //
-                                          B00001101, B01110000, //
-                                          B00011011, B10100000, //
-                                          B00111111, B11100000, //
-                                          B00111111, B11110000, //
-                                          B01111100, B11110000, //
-                                          B01110000, B01110000, //
-                                          B00000000, B00110000};
+// An SSD1306 display connected to I2C (SDA, SCL pins)
+Adafruit_SSD1306 display(128, 32, &Wire, 4);
 
 void textDemo() {
+  if (1) {
+    static const uint8_t kGlyphWidth = 6;
+    static const uint8_t kGlyphHeight = 8;
+    static const uint16_t kPerRow = 128 / kGlyphWidth;
+    static const uint16_t kRows = 32 / kGlyphHeight;
+    for (uint16_t i = 0; i < 256; ++i) {
+      uint16_t x = 0 + kGlyphWidth * (i % kPerRow);
+      uint16_t y = 0 + kGlyphHeight * ((i / kPerRow) % kRows);
+      if (x == 0 && y == 0) {
+        display.display();
+        delay(1000);
+      }
+      display.drawChar(x, y, (i & 0xff),
+                       (i & 1) == 0 ? SSD1306_WHITE : SSD1306_BLACK,
+                       (i & 1) == 1 ? SSD1306_WHITE : SSD1306_BLACK, 1);
+      // display.printf("%d,", i);
+      Serial.printf("%d\n", i);
+    }
+  }
+
+  const GFXfont *fonts[] = {
+      nullptr,                // classic
+      &FreeMonoBold12pt7b,    //
+      &FreeSerifItalic12pt7b, //
+      &FreeMonoOblique9pt7b,  //
+
+      //&Org_01,
+  };
+  for (const GFXfont *font : fonts) {
+    // Set a font
+    display.clearDisplay();
+    display.setFont(font);
+    display.cp437();
+    uint16_t x = 0;
+
+    uint16_t yAdv = font ? font->yAdvance : 8;
+    uint16_t first = font ? font->first : 0;
+    uint16_t last = font ? font->last : 255;
+
+    uint16_t y = yAdv;
+
+    for (uint16_t ch = first; ch <= last; ++ch) {
+      char s[2] = {(unsigned char)ch, 0};
+      int16_t left = 0;
+      int16_t top = 0;
+      uint16_t w = 0;
+      uint16_t h = 0;
+      display.getTextBounds(s, 0, y, &left, &top, &w, &h);
+      Serial.printf("TextBounds(\"%s\",%u,%u)=(%d,%d,%u,%u)\n", //
+                    s, x, y, left, top, w, h);
+      if (x + left + w > display.width()) {
+        display.display();
+        delay(200);
+        x = 0;
+        Serial.print("clearing display\n");
+        display.clearDisplay();
+        // display.display();
+      }
+      if (top < 0) {
+        Serial.printf("adjust y=%u by %d !!!\n", y, -top);
+        delay(1000);
+        y += -top;
+      }
+      display.drawChar(x, y, ch, SSD1306_WHITE, SSD1306_BLACK, 1);
+      x += left + w;
+      // delay(20);
+    }
+  }
+}
+
+void setup() {
   Serial.begin(9600);
   if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3C for 128x32
     Serial.println(F("SSD1306 allocation failed"));
@@ -66,75 +113,6 @@ void textDemo() {
   delay(2000);
   display.clearDisplay();
   delay(1000);
-
-  if (0) {
-    static const uint8_t kGlyphWidth = 6;
-    static const uint8_t kGlyphHeight = 8;
-    static const uint16_t kPerRow = 128 / kGlyphWidth;
-    static const uint16_t kRows = 32 / kGlyphHeight;
-    for (uint16_t i = 0; i < 256; ++i) {
-      uint16_t x = 0 + kGlyphWidth * (i % kPerRow);
-      uint16_t y = 0 + kGlyphHeight * ((i / kPerRow) % kRows);
-      display.drawChar(x, y, (i & 0xff),
-                       (i & 1) == 0 ? SSD1306_WHITE : SSD1306_BLACK,
-                       (i & 1) == 1 ? SSD1306_WHITE : SSD1306_BLACK, 1);
-      // display.printf("%d,", i);
-      Serial.printf("%d\n", i);
-      display.display();
-      delay(10);
-    }
-  }
-
-  for (;;) {
-    const GFXfont *fonts[] = {
-        nullptr,                // classic
-        &FreeMonoBold12pt7b,    //
-        &FreeSerifItalic12pt7b, //
-        &FreeMonoOblique9pt7b,  //
-    };
-    for (const GFXfont *font : fonts) {
-      // Set a font
-      display.clearDisplay();
-      display.setFont(font);
-      display.cp437();
-      uint16_t x = 0;
-
-      uint16_t yAdv = font ? font->yAdvance : 8;
-      uint16_t first = font ? font->first : 0;
-      uint16_t last = font ? font->last : 255;
-
-      uint16_t y = yAdv;
-
-      for (uint16_t ch = first; ch <= last; ++ch) {
-        char s[2] = {(char)ch, 0};
-        int16_t left = 0;
-        int16_t top = 0;
-        uint16_t w = 0;
-        uint16_t h = 0;
-        display.getTextBounds(s, 0, y, &left, &top, &w, &h);
-        Serial.printf("TextBounds(\"%s\",%u,%u)=(%d,%d,%u,%u)\n", //
-                      s, x, y, left, top, w, h);
-        if (x + left + w > display.width()) {
-          display.display();
-          delay(200);
-          x = 0;
-          Serial.print("clearing display\n");
-          display.clearDisplay();
-          // display.display();
-        }
-        if (top < 0) {
-          Serial.printf("adjust y=%u by %d !!!\n", y, -top);
-          delay(1000);
-          y += -top;
-        }
-        display.drawChar(x, y, ch, SSD1306_WHITE, SSD1306_BLACK, 1);
-        x += left + w;
-        // delay(20);
-      }
-    }
-  }
 }
 
-void setup() { textDemo(); }
-
-void loop() {}
+void loop() { textDemo(); }
